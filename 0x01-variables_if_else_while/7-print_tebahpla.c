bda1b1a5e65ee4799b9bb72993fe17ba3815de24
tsegaye4410 <tsegayeurgessa2@gmail.com>
2022-12-09 10:20:23 -0800

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: 'print alphabet'
+ * Return: Always 0 (suceess)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c--)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
