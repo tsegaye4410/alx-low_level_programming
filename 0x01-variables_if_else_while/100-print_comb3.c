3de2d4cc04dece7afa90dc9a2e0cd08af91e873e
tsegaye4410 <tsegayeurgessa2@gmail.com>
2022-12-11 08:16:58 -0800

    100-print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: 'print alphabet'
+ * Return: Always 0 (suceess)
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 9; x++)
+	{
+		for (y = x + 1; y <= 9; y++)
+		{
+		putchar(x + '0');
+		putchar(y + '0');
+		}
+		if (x < 8)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
