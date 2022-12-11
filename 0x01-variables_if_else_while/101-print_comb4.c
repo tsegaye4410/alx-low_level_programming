df34520f64a0321390f8ee3771f617efbf835c5f
tsegaye4410 <tsegayeurgessa2@gmail.com>
2022-12-11 09:01:05 -0800

    101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ * Description: 'print alphabet'
+ * Return: Always 0 (suceess)
+ */
+int main(void)
+{
+	int x;
+	int y;
+	int z;
+
+	for (x = 0; x <= 7; x++)
+	{
+		for (y = x + 1; y <= 9; y++)
+		{
+			for (z = y + 1; z <= 9; z++)
+			{
+		putchar(x + '0');
+		putchar(y + '0');
+		putchar(z + '0');
+		if (x < 7)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
