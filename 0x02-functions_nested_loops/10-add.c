122f8b1f541605e66444f1e7e0b6a85cf232ca96
tsegaye4410 <tsegayeurgessa2@gmail.com>
2022-12-13 12:36:31 -0800

    update

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "mani.h"
+/**
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
