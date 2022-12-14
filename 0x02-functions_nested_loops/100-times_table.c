fce2a63b0196ebc111b153d2a1f4e6399bb1bb83
tsegaye4410 <tsegayeurgessa2@gmail.com>
2022-12-14 07:31:42 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -37,8 +37,10 @@ void print_times_table(int n)
 					_putchar(((c / 10) % 10) + '0');
 					_putchar((c % 10) + '0');
 				}
-				else 
+				else
+				{
 					_putchar((c % 10) + '0');
+				}
 			}
 			_putchar('\n');
 		}
