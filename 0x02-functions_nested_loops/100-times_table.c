c0c25c2cade5894e57750564a80a78beafc25d83
tsegaye4410 <tsegayeurgessa2@gmail.com>
2022-12-14 07:11:25 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,41 +7,37 @@ void print_times_table(int n)
 {
 	int a, b, c;
 
-	if (n <= 0 && n <= 15)
+	if (n >= 0 && n <= 15)
 	{
 		for (a = 0; a <= n; a++)
 		{
 			for (b = 0; b <= n; b++)
 			{
 				c = a * b;
-				if (b == 0)
+				if (b != 0)
 				{
-					_putchar(c + '0');
+					_putchar(',');
+					_putchar(' ');
 				}
 				else if (c < 10 && b != 0)
 				{
-					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(c + '0');
+					_putchar((c % 10) + '0');
 				}
 				else if (c >= 10 && c < 100)
-				{
-					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 					_putchar((c / 10) + '0');
 					_putchar((c % 10) + '0');
 				}
-				else if (c >= 100)
+				else if (c >= 100 && b != 0)
 				{
-					_putchar(',');
-					_putchar(' ');
 					_putchar((c / 100) + '0');
 					_putchar(((c / 10) % 10) + '0');
 					_putchar((c % 10) + '0');
 				}
+				else 
+					_putchar((c % 10) + '0');
 			}
 			_putchar('\n');
 		}
