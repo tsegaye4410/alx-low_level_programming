cd3902571826454932a9a827b0501bbdbc1b175e
tsegaye4410 <tsegayeurgessa2@gmail.com>
2022-12-15 12:40:41 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - prints the digits 1-9
+ */
+void print_numbers(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		_putchar(i + '0');
+		i++;
+	}
+	_putchar('\n');
