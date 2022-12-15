96b3d242357b0d9256b6a7a805d3a5d063e2bd39
tsegaye4410 <tsegayeurgessa2@gmail.com>
2022-12-15 13:18:49 -0800

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,11 +5,13 @@
  */
 void print_most_numbers(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i <= 9; i++)
+	while (i <= 9)
 	{
-		_putchar(i + '0');
+		if (i != 2 && i != 4)
+			_putchar(i + '0');
+		++i;
 	}
 	_putchar('\n');
 }
