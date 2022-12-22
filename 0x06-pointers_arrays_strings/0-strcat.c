507f250bcd0adb02823492d741d74d33ec4fa6b3
tsegaye4410 <tsegayeurgessa2@gmail.com>
2022-12-22 08:29:39 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <string.h>
 /**
- * strcat - Concatenates the string pointed to by @src
+ * _strcat - Concatenates the string pointed to by @src
  * @dest: A pointer to the string to be concatenated upon
  * @src: The source string to be appended to @dest
  * Return: A pointer to the destination string @dest
