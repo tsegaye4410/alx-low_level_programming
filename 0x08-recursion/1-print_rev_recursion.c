aa79165a32a3286041c1c88b7c3b10ad9650a0e4
tsegaye4410 <tsegayeurgessa2@gmail.com>
2023-01-04 09:55:39 -0800

    recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,1 +1,13 @@
 #include "main.h"
+/**
+ * _print_rev_recursion - reverse
+ * _print_rev_recursion - reverse
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
